e36725683ac16baa85733998b0cc7d6ae76d1ba0
smar152 <54095151+smar152@users.noreply.github.com>
2020-02-17 17:05:17 -0500

    Adding sass

--- src/Components/Project1/Main.js
+++ src/Components/Project1/Main.js
@@ -1,5 +1,78 @@
 import React from "react";
 
-const Main = props => <div id="project1">Geia sas</div>;
+const Main = props => (
+  <div id="project1">
+    <header id="header">
+      <nav className="navbar fixed-top bg-dark">
+        <div class="nav-item">
+          <a href="#features" className="nav-link text-light">
+            Features
+          </a>
+        </div>
+        <div className="nav-item">
+          <a href="#how-it-works" className="nav-link">
+            How it works
+          </a>
+        </div>
+        <div className="nav-item">
+          <a href="#pricing" className="nav-link">
+            Pricing
+          </a>
+        </div>
+      </nav>
+    </header>
+
+    <main className="container">
+      <div id="logo">
+        <img
+          id="header-img"
+          src="https://www.gopresto.com/i/1525439629236/n/images/presto-logo2.png"
+          alt="Presto Logo"
+        />
+      </div>
+
+      <img
+        src="https://imgix.bustle.com/uploads/image/2018/5/22/864631f4-77d7-44a6-8093-2771da573f63-wafflemaker.jpeg?w=500&h=365&fit=crop&crop=faces&auto=format&q=70"
+        alt="amazing waffle maker"
+      />
+
+      <div id="features">
+        <h2>Features</h2>
+        <p>It makes waffle bowls</p>
+        <p>That's pretty much it</p>
+      </div>
+
+      <div id="how-it-works">
+        <h2>How it works</h2>
+        <iframe
+          id="video"
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/_vsSm3ocMn8"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+
+      <div id="pricing">
+        <h1>Pricing</h1>
+        <h3>$24,57</h3>
+      </div>
+
+      <form id="form" action="https://www.freecodecamp.com/email-submit">
+        <label for="email">Sign up for our newsletter</label>
+        <input
+          type="email"
+          name="email"
+          i
+          d="email"
+          placeholder="kati@kapou.gr"
+        />
+        <input id="submit" type="submit" />
+      </form>
+    </main>
+  </div>
+);
 
 export default Main;
