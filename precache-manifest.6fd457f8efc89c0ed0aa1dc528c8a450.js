d42eaf45b888b34195ccf89e186fcfbb2a96efc9
smar152 <smar152@users.noreply.github.com>
2020-05-30 23:02:06 +0300

    Updates

---
+++ precache-manifest.6fd457f8efc89c0ed0aa1dc528c8a450.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a42ef6c08dab3dcb9f55f0618785f98",
+    "url": "/projects/index.html"
+  },
+  {
+    "revision": "bf72549e9f2477416c18",
+    "url": "/projects/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "4ebad771134a1e17119d",
+    "url": "/projects/static/css/main.82782ad4.chunk.css"
+  },
+  {
+    "revision": "bf72549e9f2477416c18",
+    "url": "/projects/static/js/2.fbbe6770.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/projects/static/js/2.fbbe6770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ebad771134a1e17119d",
+    "url": "/projects/static/js/main.8969d1f3.chunk.js"
+  },
+  {
+    "revision": "f4608244b9ed7548f307",
+    "url": "/projects/static/js/runtime-main.c418bb21.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/projects/precache-manifest.de8aa49ef69706974bdbc30c22ff63ec.js"
+  "/projects/precache-manifest.6fd457f8efc89c0ed0aa1dc528c8a450.js"
 );
 
 self.addEventListener('message', (event) => {
